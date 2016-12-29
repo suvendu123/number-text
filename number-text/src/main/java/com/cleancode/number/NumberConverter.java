9e83a8fcb8c83d43e06fa976c41b19e527586245
A952 <sbandhyopadhyay@in.tesco.com>
2016-12-29 12:04:04 +0530

    GREEN : just add condition for nine

--- number-text/src/main/java/com/cleancode/number/NumberConverter.java
+++ number-text/src/main/java/com/cleancode/number/NumberConverter.java
@@ -3,6 +3,9 @@ package com.cleancode.number;
 public class NumberConverter {
 
     public String convert(int number) {
+        if(number == 9) {
+            return "NINE";
+        }
         return number == 6 ? "SIX" : "ONE";
     }
 
