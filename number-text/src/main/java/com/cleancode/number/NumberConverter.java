1be821caaea2ec8d42a68f2a7057e82a7e0dfd83
A952 <sbandhyopadhyay@in.tesco.com>
2016-12-29 11:42:11 +0530

    GREEN : just pass the test by adding one condition

--- number-text/src/main/java/com/cleancode/number/NumberConverter.java
+++ number-text/src/main/java/com/cleancode/number/NumberConverter.java
@@ -3,6 +3,9 @@ package com.cleancode.number;
 public class NumberConverter {
 
     public String convert(int number) {
+        if(number == 6){
+            return "SIX";
+        }
         return "ONE";
     }
 
