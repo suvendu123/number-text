8da929f7b137ab0ef12af3d11c0dd68041f7da24
A952 <sbandhyopadhyay@in.tesco.com>
2016-12-29 18:18:23 +0530

    GREEN : Pass the test by adding one if condition

--- number-text/src/main/java/com/cleancode/number/NumberConverter.java
+++ number-text/src/main/java/com/cleancode/number/NumberConverter.java
@@ -1,11 +1,18 @@
 package com.cleancode.number;
 
+import static java.util.Arrays.asList;
+import java.util.List;
+
 public class NumberConverter {
 
-    private String[] singleDigitText = { "ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE" };
+    private List<String> singleDigitText = asList("ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN",
+            "EIGHT", "NINE");
 
     public String convert(int number) {
-        return singleDigitText[number];
+        if(number == 10){
+            return "TEN";
+        }
+        return singleDigitText.get(number++);
     }
 
 }
