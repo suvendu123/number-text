f70169f82ad24062c000a1264bc579088fb6a951
A952 <sbandhyopadhyay@in.tesco.com>
2016-12-29 11:35:40 +0530

    RED :  first simple test case for convert 1 to ONE

---
+++ number-text/src/main/java/com/cleancode/number/NumberConverter.java
@@ -0,0 +1,9 @@
+package com.cleancode.number;
+
+public class NumberConverter {
+
+    public String convert(int number) {
+        return "";
+    }
+
+}
---
+++ number-text/src/test/java/com/cleancode/number/NumberConverterTest.java
@@ -0,0 +1,15 @@
+package com.cleancode.number;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class NumberConverterTest {
+    
+    @Test
+    public void should_return_one(){
+        NumberConverter converter = new NumberConverter();
+        assertEquals("ONE", converter.convert(1));
+    }
+
+}
