f4d911135a39a048f51ba9263309f40886d89e6f
A952 <sbandhyopadhyay@in.tesco.com>
2016-12-29 11:44:32 +0530

    REFACTOR : Refactor the code.

--- number-text/src/main/java/com/cleancode/number/NumberConverter.java
+++ number-text/src/main/java/com/cleancode/number/NumberConverter.java
@@ -3,10 +3,7 @@ package com.cleancode.number;
 public class NumberConverter {
 
     public String convert(int number) {
-        if(number == 6){
-            return "SIX";
-        }
-        return "ONE";
+        return number == 6 ? "SIX" : "ONE";
     }
 
 }
--- number-text/src/test/java/com/cleancode/number/NumberConverterTest.java
+++ number-text/src/test/java/com/cleancode/number/NumberConverterTest.java
@@ -2,20 +2,26 @@ package com.cleancode.number;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class NumberConverterTest {
-    
+
+    private NumberConverter converter;
+
+    @Before
+    public void setUp() {
+        converter = new NumberConverter();
+    }
+
     @Test
-    public void should_return_one(){
-        NumberConverter converter = new NumberConverter();
+    public void should_return_one() {
         assertEquals("ONE", converter.convert(1));
     }
-    
+
     @Test
-    public void should_return_six(){
-        NumberConverter converter = new NumberConverter();
+    public void should_return_six() {
         assertEquals("SIX", converter.convert(6));
     }
- 
+
 }
