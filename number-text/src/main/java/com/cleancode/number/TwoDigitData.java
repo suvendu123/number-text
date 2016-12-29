5b5fb6d85973fd88b083e32f2feb15c2f799989e
A952 <sbandhyopadhyay@in.tesco.com>
2016-12-29 19:53:55 +0530

    GREEN : add some code into TwoDigitData to make the test pass

--- number-text/src/main/java/com/cleancode/number/TwoDigitData.java
+++ number-text/src/main/java/com/cleancode/number/TwoDigitData.java
@@ -4,10 +4,18 @@ import static java.util.Arrays.asList;
 import java.util.List;
 
 public class TwoDigitData {
-    
-    public List<String> twoDigitText =  asList( "" , "TEN", "TWENTY");
+
+    private List<String> tenthDigitText = asList("", "", "TWENTY", "THIRTY", "FORTY", "FIFTY", "SIXTY", "SEVENTY",
+            "EIGHTY", "NINETY");
+
+    private List<String> twoDigitText = asList("", "TEN", "ELEVEN", "TWELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN",
+            "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN");
 
     public String convert(int number) {
-         return twoDigitText.get((number/10)%10);
+        int tenthPlace = (number / 10) % 10;
+        if (tenthPlace == 1) {
+            return twoDigitText.get(number % 10 + tenthPlace);
+        }
+        return tenthDigitText.get(tenthPlace);
     }
 }
